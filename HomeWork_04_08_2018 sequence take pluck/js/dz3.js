b3838a38dd05369f0d23c46e86ed269a1646933f
Victor Andreichenko <anvictor0@gmail.com>
2018-08-05 19:49:26 +0300

    PROG HomeWorks
    1st & 2nd HomeWorks

---
+++ HomeWork_04_08_2018 sequence take pluck/js/dz3.js
@@ -0,0 +1,92 @@
+// ВОПРОС 1:
+// Напиши функцию создания генератора sequence(start, step). Она при вызове возвращает другую функцию-генератор, которая при каждом вызове дает число на 1 больше, и так до бесконечности. Начальное число, с которого начинать отсчет, и шаг, задается при создании генератора. Шаг можно не указывать, тогда он будет равен одному. Начальное значение по умолчанию равно 0. Генераторов можно создать сколько угодно.
+
+
+try {
+
+    var sequence = function (start, step) {
+        var a = start;
+        return function () {
+            a += step;
+            return a - step;
+
+        }
+    }
+
+    var generator = sequence(10, 3);
+    var generator2 = sequence(7, 1);
+    console.log(generator()); // 10 !!!!!!
+    console.log(generator()); // 13
+    console.log(generator2()); // 7
+    console.log(generator()); // 16
+    console.log(generator2()); // 8
+
+
+// Также, нужна функция take(gen, x) которая вызвает функцию gen заданное число (x) раз и возвращает массив с результатами вызовов.
+
+    function take(gen, x) {
+        var resArr = [];
+        for (var i = 0; i < x; i++) {
+            resArr.push(gen())
+        }
+        return resArr;
+    }
+
+
+    var generator3 = sequence(0, 2);
+// с учетом вызовов ранее
+    console.log(take(generator, 3)); // [19, 22, 25]
+
+// с учетом вызовов ранее
+    console.log(take(generator2, 4)); // [9, 10, 11, 12]
+
+// вызов впервые
+    console.log(take(generator3, 5)); // [0, 2, 4, 6, 8 ]
+
+
+// ВОПРОС 2:
+// напиши функцию pluck, которая берет массив объектов и возвращает массив значений определенного поля:
+
+
+    var characters = [
+        {'name': 'barney', 'age': 36},
+        {'name': 'fred', 'age': 40}
+    ];
+
+    function pluck(string, key) {
+        var resArr = [];
+        for (let i = 0; i < string.length; i++) {
+            let record = string[i];
+            if (!record[key]) {
+                return 'Такого ключа нет'
+            }
+            resArr.push(record[key]);
+        }
+        return rensArr;
+    }
+
+    console.log(pluck(characters, 'name')); // ['barney', 'fred']
+    console.log(pluck(characters, 'age')); // [36, 40]
+    console.log(pluck(characters, 'nage')); // Такого ключа нет
+
+} catch (e) {
+    console.log('Случилась ошибка - '+ e + ', надо погуглить!');
+
+    window.open("http://www.google.com.ua/search?source=ig&hl=ru&q="+e);
+}
+console.log("скрипт не «падает»");
+// при передаче несуществующего ключа, возвращать ошибку типа (Такого ключа нет)
+
+// РАЗОБРАТЬ
+// Работа с ошибками (try catch), и сама функция Error
+//
+// debugger;
+
+
+
+// var a = 10;
+// var b;
+// if(true){
+//     debugger;
+//     console.log(a);
+// }
---
+++ HomeWork_28_07_2018 iphone shop/js/dz1.js
@@ -0,0 +1,93 @@
+
+// Функция торговля
+function sale(){
+	
+	var price_mem, price_col, price, col, x; 
+	var memory = prompt('Сколько ГБ памяти  в телефоне Вы хотите?');
+	
+	while(!price_mem){
+		
+		if(memory==32){
+			price_mem=18000;
+		}else if(memory==128){
+			price_mem=19500;
+		}else if(memory==256){
+			price_mem=21500;
+		}else{
+			memory = prompt('Сколько ГБ памяти  в телефоне Вы хотите?\nПодсказка, число (32-128-256)');
+		}
+	}
+	
+
+	var color = prompt('Какого цвета телефон Вы хотите?');
+	while(!price_col){
+		if(color=='"Чёрный оникс"'){
+			price_col=40;
+			col='"Чёрный оникс"';
+			x=-31;
+		}else if(color=='Чёрный оникс'){
+			price_col=40;
+			col='"Чёрный оникс"';
+			x=-31;
+		}else if(color=='"Черный оникс"'){
+			price_col=40;
+			col='"Чёрный оникс"';
+			x=-31;
+		}else if(color=='Черный оникс'){
+			price_col=40;
+			col='"Чёрный оникс"';
+			x=-31;
+		}else if(color=='оникс'){
+			price_col=40;
+			col='"Чёрный оникс"';
+			x=-31;
+		}else if(color=='Чёрный'){
+			price_col=60;
+			col='Чёрный';
+			x=784;
+		}else if(color=='Черный'){
+			price_col=60;
+			col='Чёрный';
+			x=784;
+		}else if(color=='Серебристый'){
+			price_col=80;
+			col='Серебристый';
+			x=578;
+		}else if(color=='Золотой'){
+			price_col=100;
+			col='Золотой';
+			x=370;
+		}else if(color=='"Розовое золото"'){
+			price_col=120;
+			col='"Розовое золото"';
+			x=162;
+		}else if(color=='Розовое золото'){
+			price_col=120;
+			col='"Розовое золото"';
+			x=162;
+		}else{color = prompt('Какого цвета телефон Вы хотите?\n"Чёрный оникс", Чёрный, Серебристый, Золотой, "Розовое золото"?\nПо умолчанию - Чёрный', 'Чёрный');}
+	}
+	price = price_mem+price_col;
+
+	document.write('<br>Вы выбрали память - ', memory, ' Гб, Цена = ', price_mem, ' грн.' );
+	document.write('<br>Вы выбрали цвет - ', col, ', Цена = +', price_col, ' грн.' );
+	document.write('<br>Цена телефона = ', price, ' грн.');
+	document.write('<div style="background-image: url(img/iphone.jpg);    width: 135px;    height: 291px;    background-position: '+x+'px 0px;"></div>');
+
+	
+
+
+}
+
+
+
+// описание функции отрисовки контента после загрузки окна браузера
+var _show_condition = function(){ document.write('<h1> Задание:</h1>	<p>Спросить покупателя мобильного телефона память и цвет.<br>		Разная память и цвет - разная цена.<br>		Обработать ошибочные нажатия и отказы.	</p>	<a onclick="sale()" href="#"> <button>Старт покупки!</button></a>');}
+
+if(window.LOADED)
+    _show_condition();
+else if (window.addEventListener)
+    window.addEventListener('load', _show_condition, false); 
+else if (window.attachEvent)
+    window.attachEvent('onload', _show_condition);
+
